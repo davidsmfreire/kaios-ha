3a052dddb822c0926e0f75bda1876b1d4f648d2c
David Freire <davidsmfreire@gmail.com>
2023-07-30 23:57:14 +0100

    formatting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import 'kaios-gaia-l10n';
 import './index.css';
-import { runHomeAssistantService, HomeAssistantService} from './home_assistant';
+import { runHomeAssistantService, HomeAssistantService } from './home_assistant';
 
 const KEY_TO_SERVICE = {
     "1": HomeAssistantService("script/turn_on", "garage_door"),
-    "2": HomeAssistantService("switch/toggle", "garage_led" ),
+    "2": HomeAssistantService("switch/toggle", "garage_led"),
     // ...
 }
 
